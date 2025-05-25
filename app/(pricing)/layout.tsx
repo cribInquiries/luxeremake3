3e4a457c8fb392007d0bff0d2396fbb4fe1d96d3
Anjesh <bhattaraianjesh123@gmail.com>
2025-05-25 11:46:25 +0930

    grind time

--- app/(pricing)/layout.tsx
+++ app/(pricing)/layout.tsx
@@ -3,16 +3,12 @@ import Image from "next/image";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer";
 
-
 const Layout = ({ children }: { children: React.ReactNode }) => {
-
-
   return (
     <>
-    
       <Navbar />
       <div>{children}</div>
-      
+
       <Footer />
     </>
   );
