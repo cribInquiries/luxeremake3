fbc861871a9e7beeac4a22c630efd1e097c04cd7
Anjesh <bhattaraianjesh123@gmail.com>
2025-05-30 08:46:47 +0930

    adding               priority                     loading="eager"
                        on HouseHeroParallax

--- components/customUI/Parallax/HouseHeroParallax.tsx
+++ components/customUI/Parallax/HouseHeroParallax.tsx
@@ -7,123 +7,92 @@ import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 // your full products array
 const products = [
   {
-    title: "",
     link: "https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/2631746/pexels-photo-2631746.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/1590336/pexels-photo-1590336.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/262405/pexels-photo-262405.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/2480608/pexels-photo-2480608.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/323775/pexels-photo-323775.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/2371975/pexels-photo-2371975.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/1586298/pexels-photo-1586298.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/2462015/pexels-photo-2462015.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/2631746/pexels-photo-2631746.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/1590336/pexels-photo-1590336.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/262405/pexels-photo-262405.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/2480608/pexels-photo-2480608.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/323775/pexels-photo-323775.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/2371975/pexels-photo-2371975.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/1586298/pexels-photo-1586298.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/2462015/pexels-photo-2462015.jpeg",
   },
   {
-    title: "",
     link: "https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg",
   },
-
-  // …and any others…
 ];
 
 // custom Pexels loader (resize & compress)
@@ -170,9 +139,9 @@ export default function HouseHeroParallax() {
                   transition="all 0.5s ease"
                 >
                   <Image
-                  
                     quality={70}
-                    loader={pexelsLoader}
+                    priority
+                    loading="eager"
                     src={product.link}
                     alt={"Stunning house collage"}
                     layout="fill"
